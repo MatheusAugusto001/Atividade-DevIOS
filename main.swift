44503d310321c4f6e3b59b8377290de10377db77
Mateus Augusto <11901446@aluno.cotemig.com.br>
2021-03-01 16:33:14 -0300

    Atividade

---
+++ main.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+print("Atividade Swift")
+
+print ("Número 1")
+
+var numerodeDedos:Int=3
+print(numerodeDedos)
+
+var numdeDedos = 5
+print(numdeDedos)
+
+
+print ("Número 2")
+
+
+print("Venda de Carros")
+
+let carro: String = "dodgert"
+print(carro)
+
+let ano: Int = 2021
+print(ano)
+
+let fabricante: String = "Dodge"
+
+let tipo: Double = 4.0
+print(tipo)
+
+let cor: String = "Preto"
+print(cor)
+
+
+let rodas: Int = 4
+print(rodas)
+
+
+var km: Int = 4500
+print(km)
+
+
+var estado: String = "Semi-novo"
+print(estado)
+
+
+var placa: String = " 4NJ86C"
+print(placa)
+
+
+var ipva: Double = 3300
+print(ipva)
+
+var preço: Double = 80000
+print(preço)
+
+
+print ("Número 3")
+
+
+print("Nos tipos de variáveis estudas hojê String, Double, Int, se referem respectivamente a variáveis String usada para uma sequência de caracteres como palabras. Double para armazenar números com ponto flutuante, sendo ela maior que Int. Int armazena valores numéricos inteiros, sendo menor que Double.")
+
+
+
+print ("Número 4")
+
+var num: Int = 66
+var numeros = String(num)
+print(numeros)
+
+
+var numero: Double = 45
+var valor = String(numero)
+print(valor)
+
+
+
+print ("Número 5")
+
+
+print(" Type-Safe, é um sistema que significa onde a linguagem ajuda você a ser claro sobre os tipos dos valores com os quais seu código pode trabalhar como o Swift. Type Inference é a detecção automática do tipo de uma expressão em uma linguagem.")
+
