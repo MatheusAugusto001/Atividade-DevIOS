260b6295a48ac93f64813b4f91e565f6dad86dd0
MatheusAugusto001 <79843917+MatheusAugusto001@users.noreply.github.com>
2021-03-22 11:43:39 -0300

    Add files via upload

---
+++ 3º Atividade Desenvolvimento IOS.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+print("3º Atividade Swift")
+
+print("Número 1")
+
+print("Variáveis opcionais são um tipo variáveis que podem conter um valor ou nenhum valor.")
+
+print("Número 2")
+
+var array = Array<Int>(arrayLiteral:1, 2)
+var dicionario = Dictionary<String, Int>()
+dicionario["Matheus"] = 78
+dicionario["Maria"] = 91
+
+print("2.")
+print(array)
+print(dicionario)
+
+
+print("Número 3")
+
+print("Arrays são coleções ordenadas de valores. Sets são coleções não ordenadas de valores únicos.")
+
+
+print("Número 4")
+
+var arrayInt = Array<Int>(arrayLiteral:1, 13, 13, 26, 38, 38, 39, 41, 50, 50, 74, 74, 80, 90, 100)
+
+print("4.")
+print(arrayInt)
+
+
+print("Número 5")
+
+var total = 0
+for i in arrayInt 
+{
+    total = total + i
+}
+
+print("5.", total)
+
+
